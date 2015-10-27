39d266a8d11b24463f69fd8ac425e57fec523aac
Jannick van Ballegooijen <jvbdesignz@gmail.com>
2015-10-27 11:57:12 +0100

    added tilemap

--- Fugam/Fugam/Model/Tile/Tile.cs
+++ Fugam/Fugam/Model/Tile/Tile.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Fugam.Model.Tile
 {
-    class Tile
+    public class Tile
     {
+        public const int SIZE = 32;
+        private bool isSolid;
+        public int x { get; }
+        public int y { get; }
+        public int id { get; }
+
+        private Bitmap tileset;
+        private Bitmap tile;
+        private Rectangle rect;
+
+        public Tile(bool isSolid, int x, int y, int id)
+        {
+            this.isSolid = isSolid;
+            this.x = x;
+            this.y = y;
+            this.id = id;
+
+            //get tileset
+
+            id--;
+
+        }
     }
 }
--- Fugam/Fugam/Model/Tile/TileMap.cs
+++ Fugam/Fugam/Model/Tile/TileMap.cs
@@ -6,7 +6,41 @@ using System.Threading.Tasks;
 
 namespace Fugam.Model.Tile
 {
-    class TileMap
+    public class TileMap
     {
+        private int[][] tilemap;
+        private Tile[,] tiles;
+        public int x, y;
+        public bool collisionMap;
+
+        public TileMap(int[][] map, bool collisionMap)
+        {
+            this.tilemap = map;
+            this.y = tilemap[0].Length;
+            this.x = tilemap.Length;
+            this.collisionMap = collisionMap;
+            tiles = new Tile[x,y];
+        }
+
+        private void loadTiles(int x, int y)
+        {
+            for (int i = 0; i < y; i++)
+            {
+                for (int k = 0; k < x; k++)
+                {
+                    if (collisionMap)
+                    {
+                        if (tilemap[k][i] != 0)
+                            tiles[k,i] = new Tile(true, i * Tile.SIZE, k * Tile.SIZE, tilemap[k][i]);
+                        else
+                            tiles[k,i] = new Tile(false, i * Tile.SIZE, k * Tile.SIZE, tilemap[k][i]);
+                    }
+                    else
+                    {
+                        tiles[k,i] = new Tile(false, i * Tile.SIZE, k * Tile.SIZE, tilemap[k][i]);
+                    }
+                }
+            }
+        }
     }
 }
